b5c3d6e8aacb52e90a37eac6b0001a7ad93e4ff3
Aisina Faizrakhmanova <aisinakukmor@mail.ru>
2019-05-21 17:36:31 +0300

    Обновила классы в папке task3

--- untitled/src/ru/snatkin/task3/MultTable.java
+++ untitled/src/ru/snatkin/task3/MultTable.java
@@ -3,7 +3,7 @@ package ru.snatkin.task3;
 class MultTable {
 
     public static void main(String[] args) {
-        int i=1, n=1;
+        /*int i=1, n=1;
         while(i <= 10)
         {
             System.out.print(i + " ");
@@ -14,6 +14,14 @@ class MultTable {
             }
             i++;
             n=1;
+            System.out.println();
+        }*/
+
+        //более простое решение от Айсины
+        for (int i = 1; i <= 10; i++) {
+           for (int j = 1; j <= 10; j++) {
+               System.out.print(i * j + " ");
+           }
             System.out.println();
         }
     }
--- untitled/src/ru/snatkin/task3/Progression.java
+++ untitled/src/ru/snatkin/task3/Progression.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Progression{
      public static void main(String[] args) {
 
-         System.out.println("Введите число членов");
+         /*System.out.println("Введите число членов");
          Scanner sc = new Scanner(System.in);
 
          int N = sc.nextInt();
@@ -17,6 +17,22 @@ public class Progression{
          for (int i = 0; i < numbers.length; i++) {
              ans += numbers[i];
              System.out.println(ans);
+         }*/
+
+
+         // решение от Айсины
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("Введите первый член");
+         int first = scanner.nextInt();
+         System.out.println("Введите шаг прогрессии");
+         int b = scanner.nextInt();
+         System.out.println("Введите количество членов");
+         int count = scanner.nextInt();
+         int i = 0;
+         while (i < count) {
+             System.out.println(first);
+             first = first + b;
+             i++;
          }
      }
 }
