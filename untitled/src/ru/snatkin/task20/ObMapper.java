84800943323dc7a72045980c5aec0906269c1f26
slipsij <basealex@mail.ru>
2019-06-18 19:29:11 +0300

    task 20

---
+++ untitled/src/ru/snatkin/task20/ObMapper.java
@@ -0,0 +1,32 @@
+package ru.snatkin.task20;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+// TODO: 18.06.2019 узнать что за Exception 
+public class ObMapper {
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+        try { String str = "{\n" +
+                "  \"name\": \"http.cat\",\n" +
+                "  \"version\": \"2.0.0\",\n" +
+                "  \"priv\": true\n" +
+                "}";
+            User user = mapper.readValue(str, User.class);
+            System.out.println(user);
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
---
+++ untitled/src/ru/snatkin/task20/User.java
@@ -0,0 +1,9 @@
+package ru.snatkin.task20;
+
+import java.io.FileInputStream;
+
+public class User {
+    private String name;
+    private String version;
+    private boolean priv;
+}
